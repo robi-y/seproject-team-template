6a09218f282dacce782628f1c063cfdc6f0f546e
oshritush <oshrite2@gmail.com>
2016-05-30 19:44:52 +0300

    add controller

---
+++ controllers/app.controller.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+
+// serve angular app files from the '/app' route
+router.use('/', express.static('app'));
+
+module.exports = router;
