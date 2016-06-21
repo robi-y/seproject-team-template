d48521837b5eb7fd0ffb76fd351ecb5dcbecefef
gittypu <gy.puter.payoneer@gmail.com>
2016-06-21 15:13:44 +0300

    change

--- models/pasiont .js
+++ models/pasiont .js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var pasiontSchema = new Schema({
-    id: Number,
-    firstname: String,
+    
+    firstName: String,
 	lastName: String,
     phone: Number,	
-	email: String   
+	Email: String   
 });
 
 module.exports = pasiontSchema;
