2eda6ad3bbfb2261a1cdf2b3dad01dbe852e1717
gittypu <gy.puter.payoneer@gmail.com>
2016-06-21 15:19:37 +0300

    added id to pasiont model

--- models/pasiont .js
+++ models/pasiont .js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var pasiontSchema = new Schema({
-    
+    id:Number,
     firstName: String,
 	lastName: String,
     phone: Number,	
